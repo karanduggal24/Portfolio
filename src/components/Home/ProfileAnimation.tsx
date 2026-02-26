8308e8fc01dde82f787b0d5bdc41b5175de6a6b0
karanduggal24 <karanduggal6239@gmail.com>
2026-02-26 11:13:38 +0530

    changed text a bit in proile badge

--- src/components/Home/ProfileAnimation.tsx
+++ src/components/Home/ProfileAnimation.tsx
@@ -72,7 +72,7 @@ export const MovingProfileImage = ({
               Karan Duggal
             </h2>
             <span className="text-[10px] sm:text-xs text-secondary/80 dark:text-secondary/80">
-              Front-End Developer
+              Full Stack Developer
             </span>
           </div>
         </div>
